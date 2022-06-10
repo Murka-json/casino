53ebfe31c6d2289a109504762ba45e75a814ce8d
lehada <98217366+lehada@users.noreply.github.com>
2022-06-10 22:10:47 +0300

    Delete src directory

--- src/keyboard/private.js
+++
@@ -1,9 +0,0 @@
-import { Keyboard } from 'vk-io'
-
-export default {
-    menu_keyboard: Keyboard.keyboard([
-        [
-            Keyboard.textButton({ label: "Играть", payload: { command: "games_list" }, color: "positive" })
-        ]
-    ])
-}
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import start from './private/start.js'
-import gamesList from './private/gamesList.js'
-import modesWheel from './admin/modes/wheel.js'
-
-export default (ctx, vk, config) => {
-    start(ctx)
-    gamesList(ctx)
-    modesWheel(ctx)
-}
