f0d7414ebfada8488499090b89a33407b64a4043
lehada <98217366+lehada@users.noreply.github.com>
2022-06-11 21:21:27 +0300

    Delete src directory

--- src/games/global/balance.js
+++
@@ -1,9 +0,0 @@
-import utils from "../../../addons/utils.js"
-
-export default (ctx, user) => {
-    if(ctx.isChat && ctx.messagePayload?.command == 'balance') {
-        return ctx.send({
-            message: `[id${user.id}|${user.name}], ваш баланс ${utils.number_format(user.balance)} VkCoin${user.bbalance > 0 ? `\nБонусный баланс ${utils.number_format(user.bbalance)} VkCoin` : `` }`
-        })
-    }
-}
--- src/games/global/bank.js
+++
@@ -1,7 +0,0 @@
-export default (ctx, conv) => {
-    if(ctx.isChat && ctx.messagePayload?.command == 'bank' && conv.gameData.game == 'wheel') {
-        if(conv.gameData.amount < 1) return ctx.send({
-            message: `В этом раунде ещё никто не поставил.\n\nХеш игры: ${conv.gameData.resultData.hash}`
-        })
-    }
-}
